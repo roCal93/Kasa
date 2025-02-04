6b4ea79bbd57ebc1722cfe3d2ee624788716356a
Romain Calmelet <romaincalmelet@gmail.com>
2025-02-04 19:28:11 +0100

    h1

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -4,9 +4,9 @@ import Dh1 from '../../assets/Dh1.png'
 const Home = () => {
   return (
     <div className="home">
-      <div>
-        <img src={Dh1} />
+      <div className="home__intro">
         <h1>Chez vous, partout et ailleurs</h1>
+        <img src={Dh1} />
       </div>
       <div></div>
     </div>
