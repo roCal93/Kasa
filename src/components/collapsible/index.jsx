ceee48ea9e89a7831866c50a45a733febf9606d3
Romain Calmelet <romaincalmelet@gmail.com>
2025-02-11 19:29:59 +0100

    startAboutUs

---
+++ src/components/collapsible/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Collapsible = () => {
+  return (
+    <div>
+      <button type="button" className="collapsible">
+        Open Collapsible
+      </button>
+      <div className="content">
+        <p>Lorem ipsum...</p>
+      </div>
+    </div>
+  )
+}
+
+export default Collapsible
--- src/pages/about-us/index.jsx
+++ src/pages/about-us/index.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import Banner from '../../components/Banner'
+import React, { useState } from 'react'
+import Banner from '../../components/banner'
+import Collapsible from '../../components/collapsible'
+import { aboutList } from '../../aboutList'
 
 const AboutUs = () => {
+  const [showMore, setShowMore] = useState(false)
+  const toggleContent = (id) => {
+    setShowMore(!showMore)
+  }
   return (
     <div className="aboutUs">
       <Banner aboutUs={true} />
+      <div className="aboutUs__content">
+        <ul>
+          {aboutList.map((data) => (
+            <li key={data.id}>
+              <div>
+                <h2>{data.title}</h2>
+                <button onClick={toggleContent}>
+                  <i
+                    className={
+                      showMore
+                        ? 'fa-solid fa-chevron-down'
+                        : 'fa-solid fa-chevron-up'
+                    }
+                  ></i>
+                </button>
+              </div>
+              {showMore && <p>{data.content}</p>}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
